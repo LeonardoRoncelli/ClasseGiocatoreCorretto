07a28e69da055e253db3bc2cc2cbc8c95b4ad45e
Roncelli <leonardoroncelli@gmail.com>
2024-11-10 10:29:17 +0100

    Classe Giocatore con metodi set e get.

---
+++ src/Giocatore.java
@@ -0,0 +1,23 @@
+public class Giocatore {
+    private String nome;
+    private String capitano;
+    private int goal;
+    public void setNome(String nome){
+        this.nome=nome;
+    }
+    public String getNome(){
+        return nome;
+    }
+    public void setCapitano(String capitano){
+        this.capitano=capitano;
+    }
+    public String getCapitano(){
+        return capitano;
+    }
+    public void setGoal(int goal){
+        this.goal=goal;
+    }
+    public int getGoal(){
+        return goal;
+    }
+}
